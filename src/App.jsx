ad198ef72ebd3a6ecf4d18bb4818cf7257c7b86c
Khilesh Jawale <khilujawale321@gmail.com>
2025-09-07 23:43:42 +0530

    replace the lenis with the gsap smoothscroll for more smooth animation on low end devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import gsap from 'gsap'
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import Message from './sections/Message';
-import ReactLenis from 'lenis/react';
 import Flavor from './sections/Flavor';
 import Nutrition from './sections/Nutrition';
 import Benefits from './sections/Benefits';
@@ -12,12 +11,20 @@ import Footer from './sections/Footer';
 import PreFooter from './sections/PreFooter';
 import Loader from './components/Loader';
 import { useState, useEffect } from 'react';
+import { useGSAP } from '@gsap/react';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 1.7,
+      effects: true,
+    });
+
+  })
   const handleLoaderComplete = () => {
     setTimeout(() => {
       setIsLoading(false);
@@ -36,16 +43,19 @@ function App() {
     <div>
       {isLoading && <Loader onLoaded={handleLoaderComplete} />}
       <Navbar />
-      <ReactLenis root />
-      <Hero />
-      <Message />
-      <Flavor />
-      <Nutrition />
-      <Benefits />
-      <Testimonial />
-      <PreFooter />
-      <Footer />
-      {/* <div className="min-h-screen border border-red-400"></div> */}
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <Hero />
+          <Message />
+          <Flavor />
+          <Nutrition />
+          <Benefits />
+          <Testimonial />
+          <PreFooter />
+          <Footer />
+          {/* <div className="min-h-screen border border-red-400"></div> */}
+        </div>
+      </div>
     </div>
   )
 }
